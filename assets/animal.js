03825ff0f43bf75ee751898a812e7cabff363684
Rob <robert.koskulics@me.com>
2017-12-11 11:17:47 -0500

    More or less done

--- assets/animal.js
+++ assets/animal.js
@@ -21,13 +21,12 @@ $("#add-animal").on("click", function(event) {
     var animal = $("#animal-input").val().trim();
     // Write code to add the new movie into the animalList array
     animalList.push(animal);
-    console.log(animalList)
     renderButtons();
     });
 
 function displayAnimalGif() {
+	$("#gifs").empty();
 	var animalName = $(this).attr("data-name");
-	console.log(animalName);
 	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lzcGYta9bfMdDiwu3ZImzoJH3rgRatgC&q=" + animalName + "&limit=10&offset=0&&lang=en";
 	$.ajax({
       url: queryURL,
@@ -36,21 +35,41 @@ function displayAnimalGif() {
       // the next line says to stop the request, then run a function.
     }).done(function(response) {
     	for (var i = 0; i < response.data.length - 1; i++) {
-    		// Check to see if it pulls the right URL
-    		console.log(response.data[i].images.fixed_height_still.url)
-    		// Saves the URL in a variable to use later
-    		var imageUrl = response.data[i].images.fixed_height_still.url
+    		// Saves the URL for both still and moving GIFs into variables to use later
+    		var stillGif = response.data[i].images.fixed_height_still.url;
+    		var movingGif = response.data[i].images.fixed_height.url;
+    		// Saves the rating into a variable for later
+    		var rating = response.data[i].rating;
     		// creates an image tag to put into the HTML
     		var animalImage = $("<img>");
-    		//Adds the src attribute and url to the image tag, and alt text
-        	animalImage.attr("src", imageUrl);
+    		//Adds the src attribute and url to the image tag, and alt text, and still state
+    		animalImage.attr("data-state", "still")
+    		animalImage.attr("data-still", stillGif)
+    		animalImage.attr("data-animate", movingGif)
+        	animalImage.attr("src", stillGif);
         	animalImage.attr("alt", "animal image");
+        	animalImage.addClass("gifType");
         	// Puts the GIF on the HTML
-        	$("#gifs").prepend(animalImage);    	}
+        	$("#gifs").prepend(animalImage, rating);
+
+        }
     	
     });
 };
+// Add an on click event to change from still to moving and back
+function animate(){
+    var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate")                    
+        }else{
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still")
+        }
+    };
 // Adding click event listeners to all elements with a class of "animal"
 $(document).on("click", ".animal", displayAnimalGif);
+// Add on click even to animate gifs
+$(document).on("click", ".gifType", animate)
 // Starts the website with a few buttons
 renderButtons();
